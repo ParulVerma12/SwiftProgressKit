169208dfb8797dab1fa45822b401000896483eaa
parul-opayn <89000853+parul-opayn@users.noreply.github.com>
2024-12-29 21:28:15 +0530

    Initial Commit

---
+++ SwiftProgressKit/SwiftProgressKit.h
@@ -0,0 +1,18 @@
+//
+//  SwiftProgressKit.h
+//  SwiftProgressKit
+//
+//  Created by My Mac on 29/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftProgressKit.
+FOUNDATION_EXPORT double SwiftProgressKitVersionNumber;
+
+//! Project version string for SwiftProgressKit.
+FOUNDATION_EXPORT const unsigned char SwiftProgressKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftProgressKit/PublicHeader.h>
+
+
